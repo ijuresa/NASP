1634d508a79d5c63e05b836188004ce10d4a69e2
Ivan Juresa <ivanjuresa611@gmail.com>
2017-02-07 23:04:11 +0100

    Added: ProcessImageButton, Click Event
    Created basic usage of Click event

--- NASP_GUI/mainwindow.cpp
+++ NASP_GUI/mainwindow.cpp
@@ -21,7 +21,6 @@ MainWindow::~MainWindow() {
 
 /**
  * @brief MainWindow::on_findImageButton_clicked
- * @
  */
 void MainWindow::on_findImageButton_clicked() {
     //Get image path
@@ -42,3 +41,40 @@ void MainWindow::on_findImageButton_clicked() {
     }
 }
 
+
+
+void onMouseClick(int event, int x, int y, int flags, void* userdata) {
+    switch(event) {
+        case cv::EVENT_LBUTTONDOWN:
+            qDebug() << "Clicked at: " << x << y;
+            break;
+    }
+}
+
+void MainWindow::on_processImageButton_clicked() {
+    if(imageHolder.getCount() == 0) {
+        QMessageBox::warning(this, tr("Images not found"),
+                             tr("There aren't any images to process.\n"
+                                "Please first select Find Image Button"));
+    } else {
+
+        for(int i = 0; i < imageHolder.getCount(); i ++) {
+            cv::namedWindow("Windows", 1);
+            cv::setMouseCallback("Windows", onMouseClick, NULL);
+            for(;;) {
+                cv::imshow("Windows", imageHolder.getShrinked(i));
+
+                char k = (char)cv::waitKey(0);
+
+                if(k == 27) {
+                    qDebug() << "Entered " << k;
+                    break;
+                }
+                else if(k = 'c') {
+                    qDebug() << "Entered " << k;
+                    break;
+                }
+            }
+        }
+    }
+}
